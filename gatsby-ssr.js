041c34766b6824b452e938755c99ccd0b0f1135a
chrisrzhou <chrisrzhou@pm.me>
2021-07-31 01:13:22 -0700

    add typescript

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,6 +5,7 @@ import config from './src/system/config.js';
 import system from './src/system/index.js';
 
 export const wrapRootElement = ({element}) => {
+  // @ts-ignore faux-esm false positive
   load({h, config, system});
   return element;
 };
