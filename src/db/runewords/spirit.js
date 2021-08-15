c4f998963e854a23c03068ef9c368e05e5ad6fb1
chrisrzhou <chrisrzhou@pm.me>
2021-08-15 13:20:09 -0700

    add spirit runeword

---
+++ src/db/runewords/spirit.js
@@ -0,0 +1,33 @@
+import {
+  BasePropertyType,
+  ItemType,
+  MagicPropertyType,
+  RunewordType,
+} from '../../enums/index.js';
+import ids from '../ids/index.js';
+
+export default {
+  id: RunewordType.TalThulOrtAmn,
+  name: 'Spirit',
+  runes: [ids.Tal, ids.Thul, ids.Ort, ids.Amn],
+  types: [
+    ItemType.NecromancerHead,
+    ItemType.PaladinShield,
+    ItemType.Shield,
+    ItemType.Sword,
+  ],
+  properties: {
+    base: {
+      [BasePropertyType.RequiredLevel]: 25,
+    },
+    magic: {
+      [MagicPropertyType.AllSkillLevels]: 2,
+      [MagicPropertyType.FasterCastRate]: [25, 35],
+      [MagicPropertyType.FasterHitRecovery]: 55,
+      [MagicPropertyType.DefenseVsMissle]: 250,
+      [MagicPropertyType.Vitality]: 22,
+      [MagicPropertyType.Mana]: [89, 112],
+      [MagicPropertyType.MagicAbsorb]: [3, 8],
+    },
+  },
+};
